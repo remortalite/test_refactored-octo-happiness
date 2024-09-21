5b782b9effe074c909d39635554213850a215596
Rem M. <morrig407@gmail.com>
2024-09-21 21:50:36 +0700

    Add getChildren() and getAllParents()

--- main.py
+++ main.py
@@ -17,10 +17,22 @@ class TreeStore:
         return self.tree.get(id, None)
 
     def getChildren(self, id: int):
-        pass
+        children = []
+        for item in items:
+            if item['parent'] == id:
+                children.append(item)
+        return children
 
     def getAllParents(self, id: int):
-        pass
+        item = self.getItem(id=id)
+        parents = []
+        while True:
+            parent_id = item['parent']
+            if parent_id == 'root':
+                break
+            item = self.getItem(id=parent_id)
+            parents.append(item)
+        return parents
 
 
 if __name__ == '__main__':
@@ -35,5 +47,9 @@ if __name__ == '__main__':
         {"id": 8, "parent": 4, "type": None}
     ]
     ts = TreeStore(items)
+
     print(ts.getAll())
     print(ts.getItem(id=1))
+    print(ts.getChildren(id=4))
+    print(ts.getAllParents(id=7))
+
